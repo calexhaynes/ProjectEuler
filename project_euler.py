f6321c4a3ab22e7a97a654bf44dc99ef18df1e24
Cameron Haynes <chaynes@Josephs-MacBook-Pro.local>
2015-05-29 13:30:24 -0400

    Just added project_euler.py

---
+++ project_euler.py
@@ -0,0 +1,12 @@
+"""(1) List all the natural numbers from 0 to 10 that are multiples of 3 and 5.
+(2) List all natural numbers from 0 to 1000 that are multiples of 3 and 5, then retrun the sum.
+"""
+def if_multiple(nat_number) :
+    """Return True if it is a multiple of 3 or 5"""
+
+    return
+
+def append_to_array(myarray, item_to_append) :
+    """Appends item_to_aooend to myarray"""
+
+    return
